4c2251223e615b4d8192b4acd94f883a4a821ef5
CYC3333 <yichengchan1309@gmail.com>
2025-12-06 15:40:37 +0800

    Update database configuration settings

--- aws-live-main/config.py
+++ aws-live-main/config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.c4ol0jb0nfh9.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
-customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+customdb = "employee_db"
+custombucket = "cycbucketcyc"
 customregion = "us-east-1"
 
